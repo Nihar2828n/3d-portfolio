081b3e080b575a604aac8b67c4c421ba7613cba7
NIHAR Vasavada <129145285+Nihar2828n@users.noreply.github.com>
2026-02-08 14:48:59 +0530

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,12 +1,10 @@
 const config = {
   title: "Nihar Vasavada | learning ",
   description: {
-    long: "I’m a 12th-grade student exploring the world of programming and web development.
-Currently learning Python and frontend basics, and building skills step by step
-with a strong interest in creating clean and interactive websites.",
-    short:
-      "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
-  },
+    description: {
+  long: "I’m a 12th-grade student exploring the world of programming and web development. Currently learning Python and frontend basics, and building skills step by step with a strong interest in creating clean and interactive websites.",
+  short: "Discover the portfolio of Nihar Vasavada."
+},
   keywords: [
     "Nihar",
     "portfolio",
